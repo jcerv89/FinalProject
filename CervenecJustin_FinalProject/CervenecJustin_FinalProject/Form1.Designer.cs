cb1f01e8f78b540c34e094f4d1bcbbd700a0d147
jcerv89 <44447967+jcerv89@users.noreply.github.com>
2019-09-25 10:38:51 -0400

    Intial Commit
    Connects to database. Tested first API connection with success. Layout of form/ interface started.

---
+++ CervenecJustin_FinalProject/CervenecJustin_FinalProject/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace CervenecJustin_FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textInfo = new System.Windows.Forms.TextBox();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textInfo
+            // 
+            this.textInfo.Location = new System.Drawing.Point(96, 742);
+            this.textInfo.Name = "textInfo";
+            this.textInfo.Size = new System.Drawing.Size(202, 31);
+            this.textInfo.TabIndex = 1;
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(75, 812);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(145, 75);
+            this.btnRandom.TabIndex = 2;
+            this.btnRandom.Text = "Random Math Facts";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(78, 260);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(520, 329);
+            this.listBox1.TabIndex = 3;
+            // 
+            // textNumber
+            // 
+            this.textNumber.Location = new System.Drawing.Point(140, 678);
+            this.textNumber.Name = "textNumber";
+            this.textNumber.Size = new System.Drawing.Size(100, 31);
+            this.textNumber.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(661, 1280);
+            this.Controls.Add(this.textNumber);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.textInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textInfo;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textNumber;
+    }
+}
+
---
+++ CervenecJustin_FinalProject/CervenecJustin_FinalProject/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace CervenecJustin_FinalProject
+{
+
+    public partial class Form1 : Form
+    {
+        DataTable theData = new DataTable();
+        MySqlConnection conn = new MySqlConnection();
+
+        int number;//vsriable for counting selected stock index
+        //variables used during process
+        WebClient aipConn = new WebClient();
+        //string to hold start of API
+        string startAPI = "http://numbersapi.com/random/math?json";
+        //string to hold completed API
+        string apiEndPoint;
+        List<Data> numbersList = new List<Data>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            string connString = DbUtilities.BuildConnection();
+
+            conn = DbUtilities.Connect(connString);
+            HandleClientWindowSize();
+        }
+        //Place this inside the class space in the form you would like to size.
+        //Call this method AFTER InitializeComponent() inside the form's constructor.
+        void HandleClientWindowSize()
+        {
+            //Modify ONLY these float values
+            float HeightValueToChange = 1.4f;
+            float WidthValueToChange = 6.0f;
+
+            //DO NOT MODIFY THIS CODE
+            int height = Convert.ToInt32(Screen.PrimaryScreen.WorkingArea.Size.Height / HeightValueToChange);
+            int width = Convert.ToInt32(Screen.PrimaryScreen.WorkingArea.Size.Width / WidthValueToChange);
+            if (height < Size.Height)
+                height = Size.Height;
+            if (width < Size.Width)
+                width = Size.Width;
+            this.Size = new Size(width, height);
+            this.Size = new Size(376, 720);
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            //string startAPI = "http://numbersapi.com/random/math?json";
+            //string to hold completed API
+            string apiEndPoint;
+
+            //Array for the Stock symbols from the listview
+            string[] dataSymbols = new string[] { };
+            numbersList.Clear();
+            //array for stocks selected to get
+            int[] dataArray = new int[listBox1.SelectedItems.Count];
+
+
+            for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+            {
+                dataArray[i] = listBox1.SelectedIndices[i];
+            }
+            string[] APisymbol = new string[dataArray.Length];
+
+            for (int n = 0; n < dataArray.Length; n++)
+            {
+                APisymbol[n] = dataSymbols[dataArray[n]];
+            }
+
+            string[] arrayStock = APisymbol;
+
+            string apiString = string.Join(",", APisymbol);
+
+            apiEndPoint = startAPI + apiString;
+
+            textInfo.Text = apiEndPoint;
+            try
+            {
+               
+
+                //Checks for internet connection
+                var apiData = aipConn.DownloadString(apiEndPoint);
+
+                //Object to parse Api data pulled for variables below
+                JObject JSONOB = JObject.Parse(apiData);
+
+               
+                
+                    Data numberData = new Data();
+                    string number;
+                    string info;
+                  
+                    //gets the json object for the data
+                   
+                    info = JSONOB["text"].ToString();
+                  number = JSONOB["number"].ToString();
+
+                    decimal num;
+                    decimal.TryParse(number, out num);
+                    numberData.Number = num;
+                    numberData.Info = info;
+
+
+               
+                
+                    //Adds data to List from pulled stock information.
+                    numbersList.Add(numberData);
+                listBox1.Items.Add(info);
+                textNumber.Text = number;
+            }
+            catch
+            {
+
+            }
+           
+            //textInfo.Text = numbersList[0].Info;
+            //numericUpDownNumber.Value = numbersList[0].Number;
+        }
+    }
+}
+
